4b1763b239deef17fc2bc7d10947e869a4615075
shafaghrastegari <shafaghrastegari@gmail.com>
2021-06-11 16:32:46 +0430

    in object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Shafagh Rastegari",
+    age: "22",
+    university: "IUST",
+  })
+  const namehandeler = () => {
+    setPerson({ ...person, university:"Iran university of science and technology" })
+  }
+  return <>
+    <h3>{person.name}</h3>
+    <h3>{person.age}</h3>
+    <h3>{person.university}</h3>
+    <button className="btn" onClick={()=>namehandeler()}>Change uni name</button>
+  </>;
 };
 
 export default UseStateObject;
