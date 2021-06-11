22169b2fd2bfb26076f8092780c21b1638721e56
shafaghrastegari <shafaghrastegari@gmail.com>
2021-06-11 15:14:31 +0430

    Basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("Shafagh");
+  const clickHandler = () => {
+    if(text === "Shafagh")
+      setText('Shafagh Rastegari')
+    else
+      setText("Shafagh")
+  }
+  return (
+  <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={clickHandler}>Change Name</button>
+    </React.Fragment>
+  )
 };
 
 export default UseStateBasics;
