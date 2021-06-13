b06581da2877cf6092499e949e73b5da9cd95c2a
shafaghrastegari <shafaghrastegari@gmail.com>
2021-06-13 12:08:17 +0430

    complex increase

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  const[value,setValue] = useState(0)
+  const [value, setValue] = useState(0)
+  const complex = () => {
+    setTimeout(() => {
+      setValue((prevState) => {
+        return prevState + 1;
+      })
+    },2000)
+  }
   return <>
   <section style={{margin:"4rem 0"}}>
       <h2>counter</h2>
       <h1>{value}</h1>
       <button className="btn" onClick={()=>setValue(value-1)}>Decrease</button>
       <button className="btn" onClick={()=>setValue(0)}>Reset</button>
-      <button className="btn" onClick={()=>setValue(value+1)}>Increase</button>
+      <button className="btn" onClick={() => setValue(value + 1)}>Increase</button>
+      <h2>complex increase</h2>
+      <h1>{value}</h1>
+      <button className="btn" onClick={complex}>complex insrease</button>
   </section>
   </>;
 };
