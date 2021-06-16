d96b75b270c6c7df7ac5837ed8f47f934ca8c39b
shafaghrastegari <shafaghrastegari@gmail.com>
2021-06-16 18:35:02 +0430

    cleanup func

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,20 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth)
+  const check = () => {
+    setSize(window.innerWidth)
+  }
+  useEffect(() => {
+    window.addEventListener('resize',check);
+    // return () => {
+    //   window.removeEventListener('resize',check);
+    // };
+  },[]);
+  return <>
+    <h1>window size</h1>
+    <h1>{size} px</h1>
+  </>;
 };
 
 export default UseEffectCleanup;
